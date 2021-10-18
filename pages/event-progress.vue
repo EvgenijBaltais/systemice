c043c4124af4f81fc207194769d8c4cbe99e7bd6
Evgeny <evgenij.baltais@gmail.com>
2021-10-18 20:54:47 +0300

    1

--- pages/event-progress.vue
+++ pages/event-progress.vue
@@ -1,147 +1,117 @@
 <template>
-	<div class="wrapper event">
+	<div class="wrapper events">
+
 		<div class="bl-l fl-column">
 			<div>
-				<a href="/"><img src="@/assets/images/logo.png"></a>
+				<NuxtLink to = "/">
+					<img src="@/assets/images/logo.png">
+				</NuxtLink>
 			</div>
 			<div class="bl-menu-title">
-				<span class="company-name">ПЛОЩАДКА</span>
+				<span class="company-name">SYSTEMICE</span>
 			</div>
 			<div class="bl-pagination">
-				<span class="active-page">04 </span>
+				<span class="active-page">01 </span>
 				<span class="bl-page"> / 07</span>
 			</div>
 		</div>
-		<div class="main">
-			<div class="event-box">
-				<div class="event-preview">
-					<div class="item-dots item-service-dots"></div>
-					<div class="prev-title">
-						<span class="event-title active">Площадки</span>
-						<span class="event-title">Ивент</span>
-						<span class="event-title">Кейтеринг</span>
-						<span class="event-title">Доп.услуги</span>
-					</div>
-				</div>
-				<div class="event-arrow-right arrow-right"></div>
-				<div class="event-arrow-left arrow-left"></div>
-				<div class="progress-box">
-					<div class="event-header">
-						<div class="step step-1 next-step">
-							<h1 class="h1">Площадки</h1>
-							<span class="event-step">Шаг №1</span>
-						</div>
-						<div class="step step-2">
-							<h1 class="h1">Ивент</h1>
-							<span class="event-step">Шаг №2</span>
-						</div>
-						<div class="step step-3">
-							<h1 class="h1">Кейтеринг</h1>
-							<span class="event-step">Шаг №3</span>
-						</div>
-						<div class="step step-4">
-							<h1 class="h1">Доп.услуги</h1>
-							<span class="event-step">Шаг №4</span>
-						</div>
-					</div>
-					<div class="loader-line">
-						<div class="active-loader"></div>
-					</div>
-					<div class="event-checked content-slide">
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="hotel" checked>
-								<span class="checkbox-span"></span>
-								<span>Отель</span>
-							</label>
-							<div class="checkbox-label" >
-								<span id="star-hall">
-									<input id="star-4" type="radio" name="hallstars"/>
-									<label title="" for="star-4">
-										<span class="star-box"></span>
-									</label>
-									<input id="star-3" type="radio" name="hallstars"/>
-									<label title="" for="star-3">
-										<span class="star-box"></span></label>
+		<div class="main h-page">
+			<pageHeader/>
 
-									<input id="star-2" type="radio" name="hallstars"/>
-									<label title="" for="star-2">
-										<span class="star-box"></span></label>
+			<div class="content-box">
+				<div class="event-details">
 
-									<input id="star-1" type="radio" name="hallstars"/>
-									<label title="" for="star-1">
-										<span class="star-box"></span></label>
+					<div class="details-header">
+						<div class = "details-header-title">
+							<span class = "details-text-span">ДЕТАЛИ</span>
+							<span class = "details-step-span">Шаг № 1</span>
+						</div>
+						<div class = "details-progress-mini">
+							<div class="details-half-filled"></div>
+						</div>
+						<div class="details-body">
+							
+							<div class = "details-body-left">
+								<div class = "details-body-field">
+									<label class = "details-date-label" for="">Дата мероприятия</label>
+									<div class = "details-input-block">
+										<input type="text" class = "" class = "details-input" placeholder="дд.мм.гг" readonly="readonly">
+										<div class = "field-arrow"></div>
+									</div>
+								</div>
 
-									<input id="star-0" type="radio" name="hallstars"/>
-									<label title="" for="star-0">
-										<span class="star-box"></span></label>
-								</span>
-								<span>(Желаемое количество звезд)</span>
+								<div class = "details-body-field">
+									<label class = "details-date-label" for="">Количество гостей</label>
+									<div class = "details-input-block">
+										<input type="text" class = "" class = "details-input" placeholder="Введите кол-во" readonly="readonly">
+										<div class = "field-arrow"></div>
+									</div>
+								</div>
+
+								<div class = "details-body-field">
+									<label class = "details-date-label" for="">Формат мероприятия</label>
+									<div class = "details-input-block">
+										<input type="text" class = "" class = "details-input" placeholder="Выбрать" readonly="readonly">
+										<div class = "field-arrow"></div>
+									</div>
+								</div>
+
+								<div class = "details-body-field">
+									<label class = "details-date-label" for="">Бюджет</label>
+									<div class = "details-input-block">
+										<input type="text" class = "" class = "details-input" placeholder="Введите бюджет в рублях" readonly="readonly">
+										<div class = "field-arrow"></div>
+									</div>
+								</div>
 							</div>
-							<div class="event-check">
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Ресторан</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Лофт</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Лофт</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Конференц-зал</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Свободное пространство</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Свободное пространство</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Лофт</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Другое</span>
-							</label>
-							<label class="checkbox-label">
-								<input class="input-checkbox" type="checkbox" name="">
-								<span class="checkbox-span"></span>
-								<span>Другое</span>
-							</label>
+
+							<div class = "details-body-right">
+
+								<div class = "details-body-field">
+
+									<input type="checkbox" id="cb1"> <label for="cb1">Флажок 1</label>
+									
+								</div>
+
+								<div class = "details-body-field">
+									<label class = "details-date-label" for="">Место проведения</label>
+									<div class = "details-input-block">
+										<input type="text" class = "" class = "details-input" placeholder="Выбрать" readonly="readonly">
+										<div class = "field-arrow"></div>
+									</div>
+								</div>
+
+								<div class = "details-body-field">
+									<label class = "details-date-label" for="">Сайт компании/ссылка на соц. сети</label>
+									<div class = "details-input-block">
+										<input type="text" class = "" class = "details-input" placeholder="Вставьте ссылку" readonly="readonly">
+										<div class = "field-arrow"></div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="event-footer">
-						<span class="status-text active-save">Сохранено</span>
-						<a class="step-btn">К следующему шагу</a>
-					</div>
+
 				</div>
-				<a href="" class="get-order">Хочу просто оставить заявку</a>
 			</div>
-				<div class="footer-mob">
-					<div class="bl-pagination">
-						<span class="active-page">04 </span>
-						<span class="bl-page"> / 07</span>
-					</div>
-					<div class="bl-copyright">
-						Ⓒ2014 - 2020
-					</div>
-				</div>
 		</div>
-		<input type="hidden" id="EventPage" value="1">
+		<blRight/>
 	</div>
 </template>
+
+<script>
+
+import blRight from '@/components/bl_right'
+import pageHeader from '@/components/page_header'
+
+export default {
+	data(){
+		return {
+		}
+	},
+	components: {
+		blRight, pageHeader
+	},
+}
+
+</script>
