87bc40b121e34f4153ad527d12f348afa83329ba
Evgeny <evgenij.baltais@gmail.com>
2021-10-21 17:21:34 +0300

    1

--- pages/tenders.vue
+++ pages/tenders.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="wrapper contact">
+	<div class="wrapper tenders">
 
 		<div class="bl-l fl-column">
 			<div>
@@ -35,123 +35,150 @@
 
 					<div class = "tenders-important">
 						
-						Более 100 выигранных тендеров
-						Оперативно и качественно готовим коммерческие предложения
-						Работаем с небольшими компаниями и крупными корпорациями
+						<p class = "tenders-important-text">Более 100 выигранных тендеров
+							Оперативно и качественно готовим коммерческие предложения
+						Работаем с небольшими компаниями и крупными корпорациями</p>
 					</div>
 
 					<div class = "tenders-info">
 						
-						<p>Если Ваша компания рассматривает нас, как потенциального участника тендерных торгов, то можете не сомневаться, что мы будем достойным участником, который с высокой долей вероятности выиграет торги путем предоставления высокого качества предлагаемых услуг и максимально выгодной для вас цены. Несомненно, мы готовы принять участие в тендере не только на разовые мероприятия, но и заключаем долгосрочные договоры на обслуживание, куда зачастую включаем пункт о гарантии лучшей цены на рынке. Пришлите прямо сейчас нам описание планируемого Вами мероприятия, укажите максимально подробно все параметры и получите предварительный расчет-смету, это позволит вам убедиться, что у нас есть для вас действительно уникальное предложение.</p>
+						<p class = "tenders-info-p">Если Ваша компания рассматривает нас, как потенциального участника тендерных торгов, то можете не сомневаться, что мы будем достойным участником, который с высокой долей вероятности выиграет торги путем предоставления высокого качества предлагаемых услуг и максимально выгодной для вас цены. Несомненно, мы готовы принять участие в тендере не только на разовые мероприятия, но и заключаем долгосрочные договоры на обслуживание, куда зачастую включаем пункт о гарантии лучшей цены на рынке. Пришлите прямо сейчас нам описание планируемого Вами мероприятия, укажите максимально подробно все параметры и получите предварительный расчет-смету, это позволит вам убедиться, что у нас есть для вас действительно уникальное предложение.</p>
 					</div>
 
 				</div>
 
 				<div class = "tenders-right">
 
-					<p class = "tenders-title-big">Приглашение к тендеру</p>
+					<p class = "tenders-title-big">
+						Приглашение к тендеру
+					</p>
 
 					<p class = "tenders-garanties">Мы гарантируем наличие и предоставление всех необходимых документов, разрешений, сертификатов и лицензий, а также финансовых гарантий согласно законодательству РФ о проведении торгов.</p>
-
 					<div class = "tenders-form">
-						
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Максимальный бюджет</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите сумму в рублях"
+								placeholder="Максимальный бюджет"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Регион (или отель). если известно</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите адрес или название отеля"
+								placeholder="Регион (или отель). если известно"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Дата мероприятия</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите сумму в рублях"
+								placeholder="Дата мероприятия"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Количество гостей</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите адрес или название отеля"
+								placeholder="Количество гостей"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Процедура оплаты</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите сумму в рублях"
+								placeholder="Процедура оплаты"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Email*</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите адрес или название отеля"
+								placeholder="Email *"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Имя*</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите сумму в рублях"
+								placeholder="Имя *"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class="tenders-half-width">
-							<label class="tenders-date-label" for="">Телефон*</label>
 							<div class="tenders-input-block">
 								<input
 								type="text"
 								class="tenders-input"
-								placeholder="Укажите адрес или название отеля"
+								placeholder="Телефон *"
 								/>
+								<div class="spy-left-input"></div>
+								<div class="spy-bottom-input"></div>
+								<div class="spy-right-input"></div>
+								<div class="spy-top-input"></div>
 							</div>
 						</div>
-
 						<div class = "tenders-textarea-w">
-							<label class="tenders-date-label" for="">Комментарий</label>
-							<textarea placeholder = "Контактное лицо" class = "tenders-textarea"></textarea>
+							<textarea placeholder = "Комментарий" class = "tenders-textarea"></textarea>
+							<div class="spy-left-textarea"></div>
+							<div class="spy-bottom-textarea"></div>
+							<div class="spy-right-textarea"></div>
+							<div class="spy-top-textarea"></div>
 						</div>
-
+					</div>
+					<div class="send-info-btns">
+						<div class="warning-info">* Нажимая на кнопку, вы даете согласие на обработку персональных данных</div>
+						<div class = "tenders-send-order">
+							<span>Отправить</span>
+							<div class="spy-left-btn"></div>
+							<div class="spy-bottom-btn"></div>
+							<div class="spy-right-btn"></div>
+							<div class="spy-top-btn"></div>
+						</div>
 					</div>
 				</div>
 			</div>
-
 		</div>
 		<blRight/>
 	</div>
