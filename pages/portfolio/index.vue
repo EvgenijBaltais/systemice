4dd4e8a8946f17e34c6e757f9cea381ffe399ef6
EvgenijBaltais <evgenij.baltais@gmail.com>
2025-01-22 16:57:56 +0300

    1

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -21,43 +21,43 @@
 					<p class = "videos-info">Команда Systemice подготовила и осуществила съемки различных проектов 2021 году. Была проделана огромная работа, чтобы мероприятие состоялось в лучшем виде!</p>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/1.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/CGessuZCMgs?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/94585286cd58edb4875ee79972538c6f/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/12.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/vmVpw6MbRx0?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/e92bdd079990a831c30e6220145c0f33/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/13.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/lqtCcB2EJC0?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/aefc4c2bb2294d23e635d9e213697ea8/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/2.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/cM9RQB-Z6bg?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/005ac4a654e9032f4c7c50e65d4bac7f/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/3.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/cGky8oRRo20?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/3d776b420feb251319552be2d1908e0b/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/10.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/we0jH4TeB10?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/b2d9c51c5c3a2aab74dfa0dd4c3e0029/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/11.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/J5I4qQCiE3A?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/a603b53e615e5c87716f8307a0526a13/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/4.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/8hPYH6cHXsI?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/6d9bbf65107b0591a219ed0957ddd2d2/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/5.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/6iKz1cxNYqE?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/49cc674a490fa585768918a7fe35c4f3/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/7.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/uWYxhy7bKpE?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/1dd1734c216a9612351f598ab2c51e4a/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/8.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/GLJep_vbZqc?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/dfaac9fe33693729bc8785ba3c6f707e/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/9.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/mTUpuGWaq5o?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/52fe0f06946f25bc359d970d82998ec9" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 				<div class="videos-example" @click = "startVideo($event)" :style="{backgroundImage: `url(${require('../../assets/images/videos/6.jpg')})`}">
-					<iframe id="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://www.youtube-nocookie.com/embed/u76KtP66WZY?rel=0&fmt=18&html5=1&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+					<iframe class="YoutubeVideo" width="100%" height="100%" style = "display: block;margin: 0 auto;" data-src="https://rutube.ru/play/embed/bf327eea20a350d4f5c887bfae825d5d" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 				</div>
 			</div>
 			
