14eccd172975882d9c653fcc4fe9040d41999822
EvgenijBaltais <evgenij.baltais@gmail.com>
2026-09-04 19:25:19 +0300

    1

--- pages/service.vue
+++ pages/service.vue
@@ -102,35 +102,6 @@
             <div
             class="service-img"
             :style="{
-              backgroundImage: `url(${require('../assets/images/business-travel.jpg')})`,
-            }"
-            ></div>
-          </div>
-          <div class = "service-desc-w">
-            <div class="service-desc">
-              <h1 class="h1">Business Travel</h1>
-              <div class="line"></div>
-              <div class="spy-left"></div>
-              <div class="spy-top"></div>
-              <div class="spy-right"></div>
-              <div class="spy-bottom"></div>
-              <div class="service-hover">
-                <a href="https://systemice-business.ru/" target="_blank" class="open-btn"
-                >Открыть</a
-                >
-              </div>
-            </div>
-            <p class="bl-desc">
-              Подразделение Systemice Group, предоставляющее комплексное обслуживание
-              корпоративным клиентам и госучреждениям.
-            </p>
-          </div>
-        </div>
-        <div class="service-block">
-          <div class="img-wrapper">
-            <div
-            class="service-img"
-            :style="{
               backgroundImage: `url(${require('../assets/images/guids.jpg')})`,
             }"
             ></div>
@@ -150,63 +121,6 @@
               </div>
             </div>
             <p class="bl-desc">Мы подобрали для Вас лучших!</p>
-          </div>
-        </div>
-        <div class="service-block">
-          <div class="img-wrapper">
-            <div
-            class="service-img"
-            :style="{
-              backgroundImage: `url(${require('../assets/images/cases.jpg')})`,
-            }"
-            ></div>
-          </div>
-          <div class = "service-desc-w">
-            <div class="service-desc">
-              <h1 class="h1">КОРПОРАТИВНЫЕ КЕЙСЫ</h1>
-              <div class="line"></div>
-              <div class="spy-left"></div>
-              <div class="spy-top"></div>
-              <div class="spy-right"></div>
-              <div class="spy-bottom"></div>
-              <div class="service-hover">
-                <a
-                href="https://systemice.ru/services/corporate_cases/"
-                target="_blank"
-                class="open-btn"
-                >Открыть</a
-                >
-              </div>
-            </div>
-            <p class="bl-desc">Здесь каждый найдет выгодное предложение для себя.</p>
-          </div>
-        </div>
-        <div class="service-block">
-          <div class="img-wrapper">
-            <div
-            class="service-img"
-            :style="{
-              backgroundImage: `url(${require('../assets/images/places.jpg')})`,
-            }"
-            ></div>
-          </div>
-          <div class = "service-desc-w">
-            <div class="service-desc">
-              <h1 class="h1">ПОДБОР МЕСТ ПРОВЕДЕНИЯ</h1>
-              <div class="line"></div>
-              <div class="spy-left"></div>
-              <div class="spy-top"></div>
-              <div class="spy-right"></div>
-              <div class="spy-bottom"></div>
-              <div class="service-hover">
-                <a href="/services/platforms/" target="_blank" class="open-btn"
-                >Открыть</a
-                >
-              </div>
-            </div>
-            <p class="bl-desc">
-              Мы гарантируем, что все площадки прошли через строгие критерии отбора.
-            </p>
           </div>
         </div>
         <div class="service-block">
