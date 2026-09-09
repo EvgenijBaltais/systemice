92008ae11da605395991232220d4d8a1d6168f5b
EvgenijB <evgenij.baltais@gmail.com>
2026-09-09 19:05:50 +0300

    1

--- pages/index.vue
+++ pages/index.vue
@@ -357,21 +357,21 @@
 								<div class="spy-bottom"></div>
 							</div>
 							<div class="bl-partner bl-partner-carousel-item">	
-								<img class="img-partner" src="@/assets/images/partners/11.png" alt="Партнёр Systemice — Инсайт Люди">
+								<img class="img-partner" src="@/assets/images/partners/14.png" alt="Партнёр Systemice — Инсайт Люди">
 								<div class="spy-left"></div>
 								<div class="spy-top"></div>
 								<div class="spy-right"></div>
 								<div class="spy-bottom"></div>
 							</div>
 							<div class="bl-partner bl-partner-carousel-item">	
-								<img class="img-partner" src="@/assets/images/partners/12.png" alt="Партнёр Systemice — Яндекс">
+								<img class="img-partner" src="@/assets/images/partners/15.png" alt="Партнёр Systemice — Яндекс">
 								<div class="spy-left"></div>
 								<div class="spy-top"></div>
 								<div class="spy-right"></div>
 								<div class="spy-bottom"></div>
 							</div>
 							<div class="bl-partner bl-partner-carousel-item">	
-								<img class="img-partner" src="@/assets/images/partners/13.png" alt="Партнёр Systemice — Росатом">
+								<img class="img-partner" src="@/assets/images/partners/16.png" alt="Партнёр Systemice — Росатом">
 								<div class="spy-left"></div>
 								<div class="spy-top"></div>
 								<div class="spy-right"></div>
